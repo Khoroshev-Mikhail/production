616bab08e2324ac4c61204dd9f4c4579b3bfb95a
Mikha38 <oyks1379@yandex.ru>
2023-01-18 13:30:47 +0300

    sql

--- server/SQL/injection.js
+++ server/SQL/injection.js
@@ -9,10 +9,45 @@ const users = fs.readFileSync(__dirname + '/users.sql', 'utf-8')
 const user_vocabulary = fs.readFileSync(__dirname + '/user_vocabulary.sql', 'utf-8')
 const words = fs.readFileSync(__dirname + '/words.sql', 'utf-8')
 
-db.none(grammar)
-db.none(content_references)
-db.none(content)
-db.none(groups)
-db.none(users)
-db.none(user_vocabulary)
-db.none(words)
+try{
+    db.none(grammar)
+}catch(e){
+    console.log(e.message)
+}
+
+try{
+    db.none(content)
+}catch(e){
+    console.log(e.message)
+}
+
+try{
+    db.none(content_references)
+}catch(e){
+    console.log(e.message)
+}
+
+
+try{
+    db.none(groups)
+}catch(e){
+    console.log(e.message)
+}
+
+try{
+    db.none(users)
+}catch(e){
+    console.log(e.message)
+}
+
+try{
+    db.none(user_vocabulary)
+}catch(e){
+    console.log(e.message)
+}
+
+try{
+    db.none(words)
+}catch(e){
+    console.log(e.message)
+}
