d7fd5ad876eb82be0fc72e46c510edf4356d9c81
Mikha38 <oyks1379@yandex.ru>
2023-01-18 20:31:43 +0300

    SQL

--- server/SQL/injection.js
+++ server/SQL/injection.js
@@ -12,18 +12,21 @@ const words = fs.readFileSync(__dirname + '/words.sql', 'utf-8')
 try{
     db.none(grammar)
 }catch(e){
+    console.log('GRAMMAR')
     console.log(e.message)
 }
 
 try{
     db.none(content)
 }catch(e){
+    console.log('CONTENT')
     console.log(e.message)
 }
 
 try{
     db.none(content_references)
 }catch(e){
+    console.log('CONTENT_REFERENCES')
     console.log(e.message)
 }
 
@@ -31,23 +34,27 @@ try{
 try{
     db.none(groups)
 }catch(e){
+    console.log('GROUPS')
     console.log(e.message)
 }
 
 try{
     db.none(users)
 }catch(e){
+    console.log('USERS')
     console.log(e.message)
 }
 
 try{
     db.none(user_vocabulary)
 }catch(e){
+    console.log('USER_VOCABULARY')
     console.log(e.message)
 }
 
 try{
     db.none(words)
 }catch(e){
+    console.log('WORDS')
     console.log(e.message)
 }
