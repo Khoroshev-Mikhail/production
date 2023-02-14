55bdad7f226dd1a3212d5d733d14c960f996d0dc
Mikha38 <oyks1379@yandex.ru>
2023-02-14 16:25:18 +0300

    injections

--- server/SQL/injection.js
+++ server/SQL/injection.js
@@ -1,53 +1,26 @@
 const db = require("../db");
 const fs = require("fs"); // import { readFileSync } from 'node:fs/promises';
 
-const grammar = fs.readFileSync(__dirname + '/grammar.sql', 'utf-8')
-const content_references = fs.readFileSync(__dirname + '/content_references.sql', 'utf-8')
-const content = fs.readFileSync(__dirname + '/content.sql', 'utf-8')
-const groups = fs.readFileSync(__dirname + '/groups.sql', 'utf-8')
-const users = fs.readFileSync(__dirname + '/users.sql', 'utf-8')
-const user_vocabulary = fs.readFileSync(__dirname + '/user_vocabulary.sql', 'utf-8')
-const words = fs.readFileSync(__dirname + '/words.sql', 'utf-8')
+async function injection(){
+    try{
+        // const grammar = fs.readFileSync(__dirname + '/grammar.sql', 'utf-8')
+        const content_references = fs.readFileSync(__dirname + '/content_references.sql', 'utf-8')
+        const content = fs.readFileSync(__dirname + '/content.sql', 'utf-8')
+        const groups = fs.readFileSync(__dirname + '/groups.sql', 'utf-8')
+        const users = fs.readFileSync(__dirname + '/users.sql', 'utf-8')
+        const user_vocabulary = fs.readFileSync(__dirname + '/user_vocabulary.sql', 'utf-8')
+        const words = fs.readFileSync(__dirname + '/words.sql', 'utf-8')
 
-try{
-    db.none(grammar)
-}catch(e){
-    console.log(e.message)
+        // await db.none(grammar)
+        await db.none(words)
+        await db.none(content)
+        await db.none(groups)
+        await db.none(content_references)
+        await db.none(users)
+        await db.none(user_vocabulary)
+
+    }catch(e){
+        console.log(e.message)
+    }
 }
-
-try{
-    db.none(words)
-}catch(e){
-    console.log(e.message)
-}
-
-try{
-    db.none(content)
-}catch(e){
-    console.log(e.message)
-}
-
-try{
-    db.none(groups)
-}catch(e){
-    console.log(e.message)
-}
-
-try{
-    db.none(content_references)
-}catch(e){
-    console.log(e.message)
-}
-
-
-try{
-    db.none(users)
-}catch(e){
-    console.log(e.message)
-}
-
-try{
-    db.none(user_vocabulary)
-}catch(e){
-    console.log(e.message)
-}
+injection()
